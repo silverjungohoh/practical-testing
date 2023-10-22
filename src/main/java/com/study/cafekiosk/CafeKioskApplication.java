8cce3a4209f93c99a2ebc72a85c60e3b505234f1
grace <grace994084@gmail.com>
2023-10-23 01:29:13 +0900

    initial commit

---
+++ src/main/java/com/study/cafekiosk/CafeKioskApplication.java
@@ -0,0 +1,15 @@
+package com.study.cafekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class CafeKioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeKioskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/cafekiosk/CafeKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeKioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
